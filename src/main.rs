4aaa5149d24757a10613b02e65f88cc9adf52bd8
Iewnfod <shangzhiqiuyue@outlook.com>
2023-12-25 12:13:30 +0800

    Add update command

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::process::Command;
 use std::string::ToString;
 use actix_files::NamedFile;
 use actix_web::{App, HttpRequest, HttpServer, web};
-use clap::Parser;
+use clap::{Parser, Subcommand};
 use lazy_mut::lazy_mut;
 use users::{get_current_uid, get_user_by_uid};
 use users::os::unix::UserExt;
@@ -43,37 +43,59 @@ struct Args {
 
     #[arg(long, default_value = "127.0.0.1")]
     host: String,
+
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    Update {}
+}
+
+fn update(current_dir: &PathBuf, current_dir_str: &String) -> Result<(), String> {
+    let target_path = current_dir.join("fluffychat-web.tar.gz");
+    let target_path_str = target_path.as_os_str().to_str().unwrap().to_string();
+
+    if target_path.exists() {
+        remove_file(&target_path).unwrap();
+    }
+
+    println!("Downloading Resources...");
+    if !data::download(current_dir_str).success() {
+        return Err("Download failed!".to_string());
+    }
+
+    println!("Extracting Resources...");
+    if !data::extract(&target_path_str, current_dir_str).success() {
+        return Err("Extract failed!".to_string());
+    }
+
+    Ok(())
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let args = Args::parse();
-
     let current_user = get_user_by_uid(get_current_uid()).unwrap();
     let home_dir = current_user.home_dir();
     let current_dir = home_dir.join("Library").join("Application Support").join(APP_ID);
     if !current_dir.exists() { create_dir_all(&current_dir).unwrap(); }
     let current_dir_str = current_dir.as_os_str().to_str().unwrap().to_string();
     let web_dir = current_dir.join("build").join("web");
+
+    let args = Args::parse();
+    if let Some(command) = args.command {
+        match command {
+            Commands::Update {} => {
+                update(&current_dir, &current_dir_str).unwrap();
+            }
+        }
+    }
+    
     unsafe { WEB_DIR = lazy_mut::LazyMut::Value(web_dir.as_os_str().to_str().unwrap().to_string()); }
     // 如果不存在，那就下载
     if !web_dir.exists() {
-        let target_path = current_dir.join("fluffychat-web.tar.gz");
-        let target_path_str = target_path.as_os_str().to_str().unwrap().to_string();
-
-        if target_path.exists() {
-            remove_file(&target_path).unwrap();
-        }
-
-        println!("Downloading Resources...");
-        if !data::download(&current_dir_str).success() {
-            return Err(std::io::Error::new(std::io::ErrorKind::Other, "Download failed!"));
-        }
-
-        println!("Extracting Resources...");
-        if !data::extract(&target_path_str, &current_dir_str).success() {
-            return Err(std::io::Error::new(std::io::ErrorKind::Other, "Extract failed!"));
-        }
+        update(&current_dir, &current_dir_str).unwrap();
     }
 
     let host = args.host;
